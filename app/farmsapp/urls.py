5c1f69169721568811aaeaf11ca48095bdd6b730
kaylalatorre <kayla_latorre@dlsu.edu.ph>
2021-10-28 22:12:40 +0800

    WIP: add-checklist, add-activity templates

--- app/farmsapp/urls.py
+++ app/farmsapp/urls.py
@@ -6,4 +6,7 @@ from . import views
 urlpatterns = [
     path('farms', views.farms, name="farms"),
     path('add-farm', views.addFarm, name="addFarm"),
+    path('biosecurity', views.biosecurity, name="biosecurity"),
+    path('add-checklist', views.addChecklist, name="addChecklist"),
+    path('add-activity', views.addActivity, name="addActivity"),
 ]
