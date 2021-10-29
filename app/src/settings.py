3a8c4823601405750dbf42060cafd48e5ba2184a
kaylalatorre <kayla_latorre@dlsu.edu.ph>
2021-10-29 16:05:25 +0800

    frontend: created "Add Checklist" template

--- app/src/settings.py
+++ app/src/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'src.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'sidcDB',
+        'USER': 'sidcdbuser',
+        'PASSWORD': 'sidcdb123',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
