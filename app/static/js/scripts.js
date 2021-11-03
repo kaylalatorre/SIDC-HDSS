c6317c79cd637ea0587d9b7e9dabddf13b5118b7
kaylalatorre <kayla_latorre@dlsu.edu.ph>
2021-11-03 12:56:48 +0800

    frontend: finished "Technician Assignment" template

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -1,4 +1,38 @@
-// This links to a temporary navigation to template
-document.getElementById("viewTFarm").onclick = function () {
-    location.href = "/tech-selected-farm";
-};
+/* Tech assignment edit button */
+let edit = document.querySelectorAll('.assignEdit');
+for(var i = 0; i < edit.length; i++) { 
+    edit[i].addEventListener("click", (e)=> {
+        let editParent = e.target.parentElement.parentElement.parentElement;
+        let editDropdown = editParent.querySelector(".form-select");
+        let assignSave = editParent.querySelector(".assignSave");
+        let assignEdit = editParent.querySelector(".assignEdit");
+
+        editDropdown.removeAttribute("disabled");
+        assignSave.setAttribute("style", "display: block");
+        assignEdit.setAttribute("style", "display: none");
+    })
+}
+
+let save = document.querySelectorAll('.assignSave');
+for(var i = 0; i < save.length; i++) { 
+    save[i].addEventListener("click", (e)=> {
+        let saveParent = e.target.parentElement.parentElement.parentElement;
+        let saveDropdown = saveParent.querySelector(".form-select");
+        let assignSave = saveParent.querySelector(".assignSave");
+        let assignEdit = saveParent.querySelector(".assignEdit");
+
+        saveDropdown.setAttribute("disabled", true);
+        assignSave.setAttribute("style", "display: none");
+        assignEdit.setAttribute("style", "display: block");
+    })
+}
+
+function viewFarm() {
+    // Note: This links to a temporary navigation to template
+        // not sure if this can be used with actual implementation? with data
+    let viewFarm = document.querySelector('#viewFarm');
+    viewFarm.onclick = function () {
+        location.href = "/tech-selected-farm";
+    };
+}
+
