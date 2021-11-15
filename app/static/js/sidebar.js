69776c1e51c23b42c431f3c079e5c8a3c571033a
kaylalatorre <kayla_latorre@dlsu.edu.ph>
2021-11-15 20:33:40 +0800

    frontend: sidebar - saving state; combined all sidebar into one

--- app/static/js/sidebar.js
+++ app/static/js/sidebar.js
@@ -1,34 +1,91 @@
-/* Toggling the dropdown menu */
-let arrow = document.querySelectorAll('.arrow');
-for(var i =0; i < arrow.length; i++) {
-    arrow[i].addEventListener("click", (e)=> {
-        let arrowParent = e.target.parentElement.parentElement;
-        arrowParent.classList.toggle("showMenu");
-    })
+var $toggleBtn = $('#toggleBtn'); // get menu button from topbar
+var $pushSelectors = $('.sidebar') // get the sidebar and content
+var sidebarIsOpen; // global variable to store sidebar state
+var openOnLoad = false; // default state
+
+/**
+ * Checking sessionStorage.
+ */
+function checkStorage() {
+    // sidebar state
+    if(sessionStorage.getItem('sidebar') === "opened") {
+        $(document).ready(function() {
+            $($pushSelectors.removeClass('close'));
+        });
+    } else {
+        console.log('Sidebar is closed.');
+    }
+
+    // nav-item states
+    if(sessionStorage.getItem('listID') != null) {
+        var val = sessionStorage.getItem('listID');
+        $('#' + val).addClass('nav-current');
+    }
 }
 
-/* Toggling sidebar */
-let sidebar = document.querySelector('.sidebar');
-let sidebarBtn = document.querySelector('.bx-menu');
-sidebarBtn.addEventListener("click", ()=> {
-    sidebar.classList.toggle("close");
-    sidebar.classList.remove("rp-close");
-})
-
-/* Toggling responsive sidebar */
-let sidebarClose = document.querySelector('#sidebar-close');
-sidebarClose.addEventListener("click", ()=> {
-    sidebar.classList.add("rp-close");
-    sidebar.classList.add("close");
-
-})
-
-/* For currently selected nav -- not yet working */
-let navItems = document.querySelectorAll('.nav-item');
-for(var i =0; i < arrow.length; i++) {
-    navItems[i].addEventListener("click", (e)=> {
-        let current = e;
-        current.classList.toggle("nav-item nav-current");
-    })
+/**
+ * Saving active link to the sessionStorage.
+ * @param {id of selected list} id 
+ */
+function active(id) {
+    sessionStorage.removeItem('listID'); //clear previous data
+    sessionStorage.setItem("listID", id); //add data to storage
+    console.log(id);
 }
 
+/**
+ * Toggling sidebar and saving state.
+ * This saves the sidebar states in a sessionStorage so that
+ * it will stay the same even when the page refreshes.
+ */
+function toggleSidebar() {
+    sidebarIsOpen = !sidebarIsOpen; 
+    
+    // check if the sidebar should be closed/open
+    if(sidebarIsOpen) {
+        $pushSelectors.removeClass('close');
+        sessionStorage.setItem('sidebar', 'opened');
+        console.log("Local Storage // sidebar: opened");
+    } else {
+        $pushSelectors.addClass('close');
+        sessionStorage.setItem('sidebar', 'closed');
+        console.log('Local Storage // sidebar: closed');
+    }
+
+    // check if the storage item exists
+    if(sessionStorage.getItem('sidebar') === null) {
+        sidebarIsOpen = openOnLoad;
+        console.log('The default state is ' + openOnLoad);
+    } else {
+        // if it exist, set value to open
+        if(sessionStorage.getItem('sidebar') === "opened") {
+            sidebarIsOpen = true;
+            console.log('Local Storage // sidebar: opened');
+        } else {
+            sidebarIsOpen = false;
+            console.log('Local Storage // sidebar: closed');
+        }
+    }
+
+    if (sidebarIsOpen) {
+        $pushSelectors.removeClass('close');
+        console.log('The \'close\' class has been removed.')
+    }
+    else {
+        $pushSelectors.addClass('close');
+        console.log('The \'close\' class has been added.')
+    }
+}
+
+$toggleBtn.on('click', toggleSidebar); 
+
+/** 
+ * Toggling the sidebar dropdown menu. 
+ */ 
+ let arrow = document.querySelectorAll('.arrow');
+ for(var i =0; i < arrow.length; i++) {
+     arrow[i].addEventListener("click", (e)=> {
+         let arrowParent = e.target.parentElement.parentElement;
+         arrowParent.classList.toggle("showMenu");
+     })
+ }
