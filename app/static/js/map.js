9124558c5ef300aaabd6e2221df366442b45f93c
kaylalatorre <kayla_latorre@dlsu.edu.ph>
2021-11-16 21:05:27 +0800

    frontend: finished Dashboard except map

---
+++ app/static/js/map.js
@@ -0,0 +1,32 @@
+// $( document ).ready(function() {
+//     console.log( "ready!" );
+//     let map = Highcharts.mapChart('map-container', {
+//         //    
+//     });
+// });
+
+document.addEventListener('DOMContentLoaded', function () {
+    const chart = Highcharts.chart('map-container', {
+        chart: {
+            type: 'bar'
+        },
+        title: {
+            text: 'Fruit Consumption'
+        },
+        xAxis: {
+            categories: ['Apples', 'Bananas', 'Oranges']
+        },
+        yAxis: {
+            title: {
+                text: 'Fruit eaten'
+            }
+        },
+        series: [{
+            name: 'Jane',
+            data: [1, 0, 4]
+        }, {
+            name: 'John',
+            data: [5, 7, 3]
+        }]
+    });
+});
