800a6891bad4d32eb6f161365c6fcddd52085fc7
kaylalatorre <kayla_latorre@dlsu.edu.ph>
2021-11-22 10:49:58 +0800

    frontend/M2: finished health-symptoms and selected-health-symptoms

--- app/healthapp/urls.py
+++ app/healthapp/urls.py
@@ -6,4 +6,8 @@ from . import views
 urlpatterns = [
     path('hogs-health', views.hogsHealth, name="hogsHealth"),
     path('selected-hogs-health', views.selectedHogsHealth, name="selectedHogsHealth"),
+    path('hogs-mortality', views.hogsMortality, name="hogsMortality"),
+    path('symptoms-reported', views.symptomsReported, name="symptomsReported"),
+    path('health-symptoms', views.healthSymptoms, name="healthSymptoms"),
+    path('selected-health-symptoms', views.selectedHealthSymptoms, name="selectedHealthSymptoms"),
 ]
--- app/healthapp/views.py
+++ app/healthapp/views.py
@@ -7,3 +7,15 @@ def hogsHealth(request):
 
 def selectedHogsHealth(request):
     return render(request, 'healthtemp/selected-hogs-health.html', {})
+
+def hogsMortality(request):
+    return render(request, 'healthtemp/rep-hogs-mortality.html', {})
+
+def symptomsReported(request):
+    return render(request, 'healthtemp/rep-symptoms-reported.html', {})
+
+def healthSymptoms(request):
+    return render(request, 'healthtemp/health-symptoms.html', {})
+
+def selectedHealthSymptoms(request):
+    return render(request, 'healthtemp/selected-health-symptoms.html', {})
