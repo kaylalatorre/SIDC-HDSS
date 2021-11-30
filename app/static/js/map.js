9ef91b7b6aafd62d6dff6eb2115cc6914f805f31
kaylalatorre <kayla_latorre@dlsu.edu.ph>
2021-12-01 03:36:14 +0800

    frontend: map - displaying by layer

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -3,9 +3,9 @@
  */
 
 // set view to batangas
-var lat =  13.753916804358575;
-var long = 121.04149886683209;
-var zoom = 12;
+var lat =  13.882406389460293;
+var long = 121.23650234971542;
+var zoom = 10.5;
 
 // initialize map
 var map = L.map('map-containter').setView([lat, long], zoom);
@@ -16,41 +16,72 @@ var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 }).addTo(map);
 
 
-
 /**
  * Populate map
  */
+var dummyData = [
+    [13.757413148089206, 121.05826626908468],
+    [13.757462595380154, 121.05812012675744],
+    [13.902000369176863, 121.08865715373985],
+    [13.784762172550467, 121.03979399812283],
+    [13.756725764301308, 121.05499304024829],
+    [14.081607476118473, 121.18257206473466],
+    [13.99479206671459, 121.21170378257706],
+    [13.840806354012464, 121.1200278421035],
+    [13.826096993746729, 121.14817479977627],
+    [13.764797506521253, 121.05729027875648],
+];
 
-// style icons
-var ibIcon = L.divIcon({
-    html: "<i class='bx bxs-circle' style='color:#ff0000' ></i>"
-});
+// initialize layer groups
+var intBiosec = new L.layerGroup();
+var extBiosec = new L.layerGroup();
+var mortalityRates = new L.layerGroup();
+var symptomsRep = new L.layerGroup();
 
-// add markers and popups
-var intBiosec = L.marker([13.837938209780106, 120.98931380662845], {icon: ibIcon}).addTo(map)
-    .bindPopup(' <label class="bold-lbl">Address:</label> address <br>' +
-                '<label class="bold-lbl">Farm Code:</label> 001 <br>' + 
-                '<label class="bold-lbl">Score:</label> 10 <br>' +
-                '<label class="bold-lbl">Last Updated:</label> 1/1/201');
+// add markers and popups to layer groups
+for (var i = 0; i < dummyData.length; i++) {
+    intBiosec.addLayer(new L.circleMarker([dummyData[i][0], dummyData[i][1]], {
+        radius: 12,
+        color: 'red',
+        weight: 0,
+        fillOpacity: 0.4,
+    } ).bindPopup(  '<label class="bold-lbl">Address:</label> address <br>' +
+                    '<label class="bold-lbl">Farm Code:</label> 001 <br>' + 
+                    '<label class="bold-lbl">Score:</label> 10 <br>' +
+                    '<label class="bold-lbl">Last Updated:</label> 1/1/201'));
 
-var extBiosec = L.marker([13.901922259900022, 121.08875371280136]).addTo(map)
-    .bindPopup(' <label class="bold-lbl">Address:</label> address <br>' +
-                '<label class="bold-lbl">Farm Code:</label> 001 <br>' + 
-                '<label class="bold-lbl">Score:</label> 10 <br>' +
-                '<label class="bold-lbl">Last Updated:</label> 1/1/201');
+    extBiosec.addLayer(new L.circleMarker([dummyData[i][0], dummyData[i][1]], {
+        radius: 8,
+        color: 'blue',
+        weight: 0,
+        fillOpacity: 0.4,
+    }).bindPopup(   '<label class="bold-lbl">Address:</label> address <br>' +
+                    '<label class="bold-lbl">Farm Code:</label> 001 <br>' + 
+                    '<label class="bold-lbl">Score:</label> 10 <br>' +
+                    '<label class="bold-lbl">Last Updated:</label> 1/1/201'));
+    
+    mortalityRates.addLayer(new L.circleMarker([dummyData[i][0], dummyData[i][1]], {
+        radius: 8,
+        color: 'yellow',
+        weight: 0,
+        fillOpacity: 0.4,
+    }).bindPopup(   '<label class="bold-lbl">Address:</label> address <br>' +
+                    '<label class="bold-lbl">Farm Code:</label> 001 <br>' + 
+                    '<label class="bold-lbl">Mortality Rate:</label> 10 <br>' +
+                    '<label class="bold-lbl">Last Updated:</label> 1/1/201'));
+    
+    symptomsRep.addLayer(new L.circleMarker([dummyData[i][0], dummyData[i][1]], {
+        radius: 8,
+        color: 'orange',
+        weight: 0,
+        fillOpacity: 0.4,
+    }).bindPopup(   '<label class="bold-lbl">Address:</label> address <br>' +
+                    '<label class="bold-lbl">Farm Code:</label> 001 <br>' + 
+                    '<label class="bold-lbl">Symptoms Reported:</label> 10 <br>' +
+                    '<label class="bold-lbl">Symptoms Active:</label> 10 <br>' +
+                    '<label class="bold-lbl">Last Updated:</label> 1/1/201'));
+}
 
-var mortalityRates = L.marker([13.756777869646523, 121.05502522675748]).addTo(map)
-    .bindPopup(' <label class="bold-lbl">Address:</label> address <br>' +
-                '<label class="bold-lbl">Farm Code:</label> 001 <br>' + 
-                '<label class="bold-lbl">Mortality Rate:</label> 10 <br>' +
-                '<label class="bold-lbl">Last Updated:</label> 1/1/201');
-
-var symptomsRep = L.marker([13.825951142071434, 121.14811042675792]).addTo(map)
-    .bindPopup(' <label class="bold-lbl">Address:</label> address <br>' +
-                '<label class="bold-lbl">Farm Code:</label> 001 <br>' + 
-                '<label class="bold-lbl">Symptoms Reported:</label> 10 <br>' +
-                '<label class="bold-lbl">Symptoms Active:</label> 10 <br>' +
-                '<label class="bold-lbl">Last Updated:</label> 1/1/201');
 
 /**
  * Layer Controls
@@ -63,8 +94,7 @@ var overlayMaps = {
     "Internal Biosecurity Scores" : intBiosec,
     "External Biosecurity Scores" : extBiosec,
     "Moratlity Rates" : mortalityRates,
-    "Symptoms Reported" : symptomsRep
+    "Symptoms Reported" : symptomsRep,
 }
 
-// add everything to map
 L.control.layers(baseMaps, overlayMaps).addTo(map);
