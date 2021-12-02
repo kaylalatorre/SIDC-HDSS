c32a2434b03981904923db41b3f617f8aa2defb6
kaylalatorre <kayla_latorre@dlsu.edu.ph>
2021-12-02 19:58:50 +0800

    frontend WIP - maps

--- app/static/js/map.js
+++ app/static/js/map.js
@@ -5,7 +5,7 @@
 // set view to batangas
 var lat =  13.882406389460293;
 var long = 121.23650234971542;
-var zoom = 10.5;
+var zoom = 10;
 
 // initialize map
 var map = L.map('map-containter').setView([lat, long], zoom);
@@ -19,64 +19,65 @@ var osm = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
 /**
  * Populate map
  */
+    // dummy data - replace one geocoding is available 
+    // lat, long, numpigs
 var dummyData = [
-    [13.757413148089206, 121.05826626908468],
-    [13.757462595380154, 121.05812012675744],
-    [13.902000369176863, 121.08865715373985],
-    [13.784762172550467, 121.03979399812283],
-    [13.756725764301308, 121.05499304024829],
-    [14.081607476118473, 121.18257206473466],
-    [13.99479206671459, 121.21170378257706],
-    [13.840806354012464, 121.1200278421035],
-    [13.826096993746729, 121.14817479977627],
-    [13.764797506521253, 121.05729027875648],
+    [13.757413148089206, 121.05826626908468, 42],
+    [13.757462595380154, 121.05812012675744, 40],
+    [13.902000369176863, 121.08865715373985, 20],
+    [13.784762172550467, 121.03979399812283, 32],
+    [13.756725764301308, 121.05499304024829, 50],
+    [14.081607476118473, 121.18257206473466, 20],
+    [13.99479206671459, 121.21170378257706, 16],
+    [13.840806354012464, 121.1200278421035, 24],
+    [13.826096993746729, 121.14817479977627, 44],
+    [13.764797506521253, 121.05729027875648, 32],
 ];
 
 // initialize layer groups
-var intBiosec = new L.layerGroup();
-var extBiosec = new L.layerGroup();
+var allFarms = new L.layerGroup();
+var pigsPerFarm = new L.layerGroup();
 var mortalityRates = new L.layerGroup();
 var symptomsRep = new L.layerGroup();
 
 // add markers and popups to layer groups
 for (var i = 0; i < dummyData.length; i++) {
-    intBiosec.addLayer(new L.circleMarker([dummyData[i][0], dummyData[i][1]], {
-        radius: 12,
-        color: 'red',
-        weight: 0,
-        fillOpacity: 0.4,
-    } ).bindPopup(  '<label class="bold-lbl">Address:</label> address <br>' +
-                    '<label class="bold-lbl">Farm Code:</label> 001 <br>' + 
-                    '<label class="bold-lbl">Score:</label> 10 <br>' +
-                    '<label class="bold-lbl">Last Updated:</label> 1/1/201'));
+    var farmLat = dummyData[i][0];
+    var farmLong = dummyData[i][1];
+    var numPigs = dummyData[i][2];
+    var radiusSize = numPigs*50;
 
-    extBiosec.addLayer(new L.circleMarker([dummyData[i][0], dummyData[i][1]], {
-        radius: 8,
-        color: 'blue',
-        weight: 0,
-        fillOpacity: 0.4,
-    }).bindPopup(   '<label class="bold-lbl">Address:</label> address <br>' +
-                    '<label class="bold-lbl">Farm Code:</label> 001 <br>' + 
-                    '<label class="bold-lbl">Score:</label> 10 <br>' +
+    allFarms.addLayer(new L.marker([farmLat, farmLong])
+    .bindTooltip(   '<label class="bold-lbl">Farm Code:</label> 001 <br>' +
+                    '<label class="bold-lbl">Address:</label> address'));
+    
+    pigsPerFarm.addLayer(new L.circle([farmLat, farmLong], {
+        radius: radiusSize,
+        color: '#FFFFFF',
+        fillColor: 'violet',
+        weight: 1,
+        fillOpacity: 0.6,
+    }).bindTooltip( '<label class="bold-lbl">Farm Code:</label> 001 <br>' + 
+                    '<label class="bold-lbl">No. of pigs: </label>' + numPigs +  '<br>' +
                     '<label class="bold-lbl">Last Updated:</label> 1/1/201'));
     
-    mortalityRates.addLayer(new L.circleMarker([dummyData[i][0], dummyData[i][1]], {
-        radius: 8,
-        color: 'yellow',
-        weight: 0,
-        fillOpacity: 0.4,
-    }).bindPopup(   '<label class="bold-lbl">Address:</label> address <br>' +
-                    '<label class="bold-lbl">Farm Code:</label> 001 <br>' + 
+    mortalityRates.addLayer(new L.circle([farmLat, farmLong], {
+        radius: 500,
+        color: '#FFFFFF',
+        fillColor: 'red',
+        weight: 1,
+        fillOpacity: 0.6,
+    }).bindTooltip( '<label class="bold-lbl">Farm Code:</label> 001 <br>' + 
                     '<label class="bold-lbl">Mortality Rate:</label> 10 <br>' +
                     '<label class="bold-lbl">Last Updated:</label> 1/1/201'));
     
-    symptomsRep.addLayer(new L.circleMarker([dummyData[i][0], dummyData[i][1]], {
-        radius: 8,
-        color: 'orange',
-        weight: 0,
-        fillOpacity: 0.4,
-    }).bindPopup(   '<label class="bold-lbl">Address:</label> address <br>' +
-                    '<label class="bold-lbl">Farm Code:</label> 001 <br>' + 
+    symptomsRep.addLayer(new L.circle([farmLat, farmLong], {
+        radius: 500,
+        color: '#FFFFFF',
+        fillColor: 'orange',
+        weight: 1,
+        fillOpacity: 0.6,
+    }).bindTooltip( '<label class="bold-lbl">Farm Code:</label> 001 <br>' + 
                     '<label class="bold-lbl">Symptoms Reported:</label> 10 <br>' +
                     '<label class="bold-lbl">Symptoms Active:</label> 10 <br>' +
                     '<label class="bold-lbl">Last Updated:</label> 1/1/201'));
@@ -91,8 +92,8 @@ var baseMaps = {
 }
 
 var overlayMaps = {
-    "Internal Biosecurity Scores" : intBiosec,
-    "External Biosecurity Scores" : extBiosec,
+    "Farms": allFarms,
+    "No. of Pigs per farm" : pigsPerFarm,
     "Moratlity Rates" : mortalityRates,
     "Symptoms Reported" : symptomsRep,
 }
