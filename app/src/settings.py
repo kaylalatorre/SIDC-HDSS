dba0837d0b2b79bb134deefe0650e16165477de9
Qijuso <kurt_go@dlsu.edu.ph>
2021-12-29 23:02:41 +0800

    Default db to cloud

--- app/src/settings.py
+++ app/src/settings.py
@@ -101,28 +101,28 @@ WSGI_APPLICATION = 'src.wsgi.application'
 # django.db.backends.postgresql_psycopg2
 
 # for LOCAL db
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.contrib.gis.db.backends.postgis',
-        'NAME': 'sidcDB',
-        'USER': 'sidcdbuser',
-        'PASSWORD': 'sidcdb123',
-        'HOST': 'localhost',
-        'PORT': '5432'
-    }
-}
-
-# for CLOUD db
 # DATABASES = {
 #     'default': {
-#         'ENGINE': os.getenv("ENGINE"),
-#         'NAME': os.getenv("DBNAME"),
-#         'USER': os.getenv("DBUSER"),
-#         'PASSWORD': os.getenv("PASSWORD"),
-#         'HOST': os.getenv("HOST"),
-#         'PORT': os.getenv("PORT")
+#         'ENGINE': 'django.contrib.gis.db.backends.postgis',
+#         'NAME': 'sidcDB',
+#         'USER': 'sidcdbuser',
+#         'PASSWORD': 'sidcdb123',
+#         'HOST': 'localhost',
+#         'PORT': '5432'
 #     }
 # }
+
+# for CLOUD db
+DATABASES = {
+    'default': {
+        'ENGINE': os.getenv("ENGINE"),
+        'NAME': os.getenv("DBNAME"),
+        'USER': os.getenv("DBUSER"),
+        'PASSWORD': os.getenv("PASSWORD"),
+        'HOST': os.getenv("HOST"),
+        'PORT': os.getenv("PORT")
+    }
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
