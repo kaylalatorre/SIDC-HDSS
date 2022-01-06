3216b7b5c0d7ea4d32a32f57205ad1cf2d715843
annakumiko <anna_kumiko_catahan@dlsu.edu.ph>
2022-01-06 23:49:54 +0800

    F7/F10: Loading date in disease monitoring report

--- app/dssapp/views.py
+++ app/dssapp/views.py
@@ -53,5 +53,81 @@ def debug(m):
 # (Module 3) Disease Monitoring view functions
 
 def diseaseMonitoring(request):
+    """
+    Gets all Incident (Hog_Symptoms) records within existing dates and all Areas due to no selected filters in dropdown
 
-    return render(request, 'dsstemp/rep-disease-monitoring.html', { })
+    (1) date today
+    (2) all Area records
+    (3) Incident details
+        - ID, Farm Code, Area, No. of Pigs Affected, Symptoms, Status, Date Reported
+    """
+
+    # for checking if filters were used in the displayed Report
+    isFiltered = False
+
+    # (1) for setting Date input filters to today's date
+    dateToday = datetime.now(timezone.utc)
+
+    # (2) all Area records
+    areaQry = Area.objects.all()
+
+    # (3.1) Incident details
+    incidQry = Hog_Symptoms.objects.select_related('ref_farm').annotate(
+        farm_code = F("ref_farm__id"),
+        farm_area = F("ref_farm__area__area_name"),
+        ).values(
+            "id",
+            "farm_code",
+            "farm_area",
+            "num_pigs_affected",
+            "report_status",
+            "date_filed"
+            ).order_by("id")
+
+    incidList = []
+    total_pigs_affect = 0
+    # total_symptoms = 0
+    for f in incidQry:
+
+        incidObject = {
+            "id":                f["id"],
+            "farm_code":         f["farm_code"],
+            "farm_area":         f["farm_area"],
+            "num_pigs_affected": f["num_pigs_affected"],
+            "report_status":     f["report_status"],
+            "date_filed":        f["date_filed"],
+        }
+        incidList.append(incidObject)
+
+    # (3.2) Incidents Reported (symptoms list)
+    symptomsList = Hog_Symptoms.objects.values(
+            'high_fever'        ,
+            'loss_appetite'     ,
+            'depression'        ,
+            'lethargic'         ,
+            'constipation'      ,
+            'vomit_diarrhea'    ,
+            'colored_pigs'      ,
+            'skin_lesions'      ,
+            'hemorrhages'       ,
+            'abn_breathing'     ,
+            'discharge_eyesnose',
+            'death_isDays'      ,
+            'death_isWeek'      ,
+            'cough'             ,
+            'sneeze'            ,
+            'runny_nose'        ,
+            'waste'             ,
+            'boar_dec_libido'   ,
+            'farrow_miscarriage',
+            'weight_loss'       ,
+            'trembling'         ,
+            'conjunctivitis').order_by("id").all()
+    
+
+    # combine the 2 previous queries into 1 temporary list
+    incident_symptomsList = zip(incidList, symptomsList)
+
+    return render(request, 'dsstemp/rep-disease-monitoring.html', {"isFiltered": isFiltered, 'dateStart': dateToday,'dateEnd': dateToday,
+                                                                    "areaList": areaQry,
+                                                                    "incident_symptomsList": incident_symptomsList})
