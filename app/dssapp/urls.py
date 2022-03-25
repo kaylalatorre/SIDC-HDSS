273a55018ab802d1c90a24d37612858d078db550
annakumiko <anna_kumiko_catahan@dlsu.edu.ph>
2022-03-25 21:46:30 +0800

    A5: Transferred weight-range to healthapp

--- app/dssapp/urls.py
+++ app/dssapp/urls.py
@@ -6,10 +6,8 @@ from . import views
 urlpatterns = [
     # dashboard - disease monitoring
     path('disease-dashboard', views.diseaseDashboard, name="diseaseDashboard"),
-    path('weight-range', views.weightRange, name="weightRange"),
     
     # reports
     path('disease-monitoring', views.diseaseMonitoring, name="diseaseMonitoring"),
     path('disease-monitoring/<str:startDate>/<str:endDate>/<str:areaName>/', views.filter_incidentRep, name="filter_incidentRep"),
-
 ]
