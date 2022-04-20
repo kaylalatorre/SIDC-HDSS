1356f5088972ddae97c724f602d8f245b024f41a
kaylalatorre <kayla_latorre@dlsu.edu.ph>
2022-04-20 20:24:42 +0800

    frontend: disease monitoring tab

--- app/static/js/disease-monitoring.js
+++ app/static/js/disease-monitoring.js
@@ -8,6 +8,47 @@ $(document).ready(async function () {
                 chart: {
                     type: 'column'
                 },
+                title: {
+                    text: ''
+                },
+                xAxis: {
+                    title: {
+                        text: 'Year'
+                    },
+                    categories: ['2018', '2019', '2020', '2021', '2022']
+                },
+                yAxis: {
+                    title: {
+                        text: 'Population Died'
+                    }
+                },
+            
+                series: [
+                    {
+                        name: 'ASF',
+                        data: [29, 5, 64, 12, 10]
+                    },
+                    {
+                        name: 'CSF',
+                        data: [9, 75, 14, 12, 14]
+                    },
+                    {
+                        name: 'IAV',
+                        data: [29, 75, 64, 12, 100]
+                    },
+                    {
+                        name: 'ADV',
+                        data: [9, 71, 106, 129, 144]
+                    },
+                    {
+                        name: 'PRRS',
+                        data: [2, 7, 10, 12, 14]
+                    },
+                    {
+                        name: 'PED',
+                        data: [14, 17, 13, 18, 20]
+                    }
+                ] 
             })
         }
     
@@ -17,6 +58,47 @@ $(document).ready(async function () {
                 chart: {
                     type: 'column'
                 },
+                title: {
+                    text: ''
+                },
+                xAxis: {
+                    title: {
+                        text: 'Year'
+                    },
+                    categories: ['2018', '2019', '2020', '2021', '2022']
+                },
+                yAxis: {
+                    title: {
+                        text: 'Population Died'
+                    }
+                },
+            
+                series: [
+                    {
+                        name: 'ASF',
+                        data: [29, 5, 64, 12, 10]
+                    },
+                    {
+                        name: 'CSF',
+                        data: [9, 75, 14, 12, 14]
+                    },
+                    {
+                        name: 'IAV',
+                        data: [29, 75, 64, 12, 100]
+                    },
+                    {
+                        name: 'ADV',
+                        data: [9, 71, 106, 129, 144]
+                    },
+                    {
+                        name: 'PRRS',
+                        data: [2, 7, 10, 12, 14]
+                    },
+                    {
+                        name: 'PED',
+                        data: [14, 17, 13, 18, 20]
+                    }
+                ] 
             })
         }
 
@@ -26,6 +108,39 @@ $(document).ready(async function () {
                 chart: {
                     type: 'spline'
                 },
+                title: {
+                    text: ''
+                },
+                legend: {
+                    layout: 'horizontal',
+                    align: 'center',
+                    verticalAlign: 'bottom',
+                    floating: false,
+                },
+                xAxis: {
+                },
+                yAxis: {
+                },
+                tooltip: {
+                    shared: true,
+                    valueSuffix: ' units'
+                },
+                series: [{
+                    name: 'Susceptible',
+                    data: [3, 4, 3, 5, 4, 10, 12]
+                }, {
+                    name: 'Exposed',
+                    data: [0, 0, 0, 0, 3, 5, 4]
+                }, {
+                    name: 'Infected',
+                    data: [1, 2, 2, 1, 3, 5, 4]
+                }, {
+                    name: 'Recovered',
+                    data: [5, 4, 10, 9, 2, 1, 1]
+                }, {
+                    name: 'Dead',
+                    data: [2, 2, 3, 4, 4, 0, 4]
+                }]
             })
         }
         
