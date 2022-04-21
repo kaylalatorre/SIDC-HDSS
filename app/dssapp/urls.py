6050e0e3997b4ec1113bccb0f47374d4dfd66c55
kaylalatorre <kayla_latorre@dlsu.edu.ph>
2022-04-21 19:38:48 +0800

    frontend: added a printable disease-monitoring ; set up action-rec

--- app/dssapp/urls.py
+++ app/dssapp/urls.py
@@ -14,6 +14,7 @@ urlpatterns = [
     path('action-recommendation', views.actionRecommendation, name="actionRecommendation"),
     
     # reports
+    path('disease-monitoring', views.diseaseMonitoring, name="diseaseMonitoring"),
     path('symptoms-monitoring', views.symptomsMonitoring, name="symptomsMonitoring"),
     path('symptoms-monitoring/<str:startDate>/<str:endDate>/<str:areaName>/', views.filter_incidentRep, name="filter_incidentRep"),
 ]
