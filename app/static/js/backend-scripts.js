52288756f510d7bc359d5e96ab50b8e07974bf89
Qijuso <kurt_go@dlsu.edu.ph>
2022-05-02 11:43:07 +0800

    Bug Fix: unable to input 0 tentative diagnosis

--- app/static/js/backend-scripts.js
+++ app/static/js/backend-scripts.js
@@ -1313,12 +1313,13 @@ $('.submit-reference').on('click', function(){
     // then assign the other variables
     let lab_ref = $(`#input-reference_${incid_id}`).val();
     let lab_result = $(`#input-positive_${incid_id}`).val() // 0 == positive, 1 == negative
-
+    console.log(lab_result);
+    console.log(Number("lol"));
     // check if inputs are valid
-    if(!Number(incid_id) || !Number(lab_ref) || !Number(lab_result) || !["ASF", "CSF", "IAVS", "ADV", "PRRS", "PED", "Others"].includes(d_name)){
+    if(!Number(incid_id) || isNaN(Number(lab_ref)) || isNaN(Number(lab_result)) || !["ASF", "CSF", "IAVS", "ADV", "PRRS", "PED", "Others"].includes(d_name)){
         console.log(!Number(incid_id));
-        console.log(!Number(lab_ref));
-        console.log(!Number(lab_result));
+        console.log(isNaN(Number(lab_ref)));
+        console.log(isNaN(Number(lab_result)));
         console.log(!["ASF", "CSF", "IAVS", "ADV", "PRRS", "PED", "Others"].includes(d_name));
         console.log("Invalid parameters were sent");
         return false;
